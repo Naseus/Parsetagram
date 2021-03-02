1cd651f441056850986e078d0c5f4c005489a6b2
Naseus <tdevries4i@gmail.com>
2021-03-01 21:22:26 -0500

    feat: login

--- app/src/main/java/com/example/parsetagram/LoginActivity.java
+++ app/src/main/java/com/example/parsetagram/LoginActivity.java
@@ -2,21 +2,61 @@ package com.example.parsetagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
     private EditText etUsername;
     private EditText etPassword;
-    private Button btnLogin
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick(login button)");
+                String user = etUsername.getText().toString();
+                String pass = etPassword.getText().toString();
+                loginUser(user, pass);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password){
+        Log.i(TAG, "Attempting to login" + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+               if(e != null) {
+                   Log.e(TAG, "issue with login", e);
+                   return;
+               }
+               toMainActivity();
+            }
+        });
+    }
+
+    private void toMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/parsetagram/ParseApplication.java
@@ -0,0 +1,20 @@
+package com.example.parsetagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.app_id))
+                .clientKey(getString(R.string.client_key))
+                .server(getString(R.string.server))
+                .build()
+        );
+    }
+}
