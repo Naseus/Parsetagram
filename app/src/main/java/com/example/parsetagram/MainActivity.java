a62b6c81c1f9000ecb221f63a65feae1fd69f59d
Naseus <tdevries4i@gmail.com>
2021-03-02 22:37:48 -0500

    feat: Uploading posts and takeing pictures

--- app/src/main/java/com/example/parsetagram/MainActivity.java
+++ app/src/main/java/com/example/parsetagram/MainActivity.java
@@ -1,34 +1,49 @@
 package com.example.parsetagram;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
-import android.media.Image;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final int CAPTURE_IMAGE_ACTIVITY_RESQUEST_CODE = 27;
     EditText etDescription;
     ImageButton ibtnTakePicture;
     Button btnPost;
-    ImageView imgPost;
+    ImageView ivPost;
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +53,82 @@ public class MainActivity extends AppCompatActivity {
         etDescription = findViewById(R.id.etDescription);
         ibtnTakePicture = findViewById(R.id.ibtnTakePicture);
         btnPost = findViewById(R.id.btnPost);
-        imgPost = findViewById(R.id.imgPost);
+        ivPost = findViewById(R.id.ivPost);
 
         queryPosts();
 
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String description = etDescription.getText().toString();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                if(photoFile == null || ivPost.getDrawable() == null)
+                    Toast.makeText(MainActivity.this, "There is no image", Toast.LENGTH_LONG).show();
+                else if(description.isEmpty())
+                    Toast.makeText(MainActivity.this, "There is no description", Toast.LENGTH_LONG).show();
+                else
+                    savePost(description, currentUser, photoFile);
+            }
+        });
+
+        ibtnTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCamera();
+            }
+        });
+    }
+
+    private void savePost(String description, ParseUser currentUser, File photoFile) {
+        Post post = new Post();
+        post.setDescription(description);
+        post.setImage(new ParseFile(photoFile));
+        post.setUser(currentUser);
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "Error while saving", e);
+                    Toast.makeText(MainActivity.this, "Error while saving.", Toast.LENGTH_LONG).show();
+                } else {
+                    Log.i(TAG, "Post saved successfully");
+                    Toast.makeText(MainActivity.this, "Post saved successfully", Toast.LENGTH_LONG).show();
+                    etDescription.setText("");
+                    ivPost.setImageResource(0);
+                }
+            }
+        });
+    }
+
+    private void launchCamera() {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        photoFile = getPhotoFileUri(photoFileName);
+        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", photoFile);
+        i.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        if(i.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(i, CAPTURE_IMAGE_ACTIVITY_RESQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_RESQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                ivPost.setImageBitmap(takenImage);
+                Log.e(TAG, "Image changed");
+            } else {
+                Toast.makeText(this, "failed to take photo", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private File getPhotoFileUri(String fileName) {
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+        if(!mediaStorageDir.exists() && !mediaStorageDir.mkdirs())
+            Log.d(TAG, "failed to create directory");
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
     }
 
     @Override
